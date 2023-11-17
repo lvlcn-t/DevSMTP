9f59530c50326f6cd9ea10ce4dd2d0e6e7b9d90f
lvlcn-t <75443136+lvlcn-t@users.noreply.github.com>
2023-11-17 22:53:50 +0100

    feat: go init project

---
+++ cmd/server/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/lvlcn-t/halog/pkg/logger"
+
+func main() {
+	_ = logger.NewLogger()
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/lvlcn-t/DevSMTP
+
+go 1.21
+
+require github.com/lvlcn-t/halog v0.1.0
