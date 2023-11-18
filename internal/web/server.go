8264a64a70f024497d475d559e9f0671f996daab
lvlcn-t <75443136+lvlcn-t@users.noreply.github.com>
2023-11-18 19:30:38 +0100

    feat: add web server

--- internal/web/server.go
+++ internal/web/server.go
@@ -1,1 +1,40 @@
 package web
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/lvlcn-t/DevSMTP/internal/config"
+	"github.com/lvlcn-t/DevSMTP/internal/web/renderer"
+)
+
+type Server interface {
+	Run() error
+}
+
+type server struct {
+	r   *gin.Engine
+	cfg *config.Config
+}
+
+func NewWebServer(cfg *config.Config) (Server, error) {
+	r := gin.New()
+	r.Use(
+		gin.LoggerWithWriter(gin.DefaultWriter, "/healthz"),
+		gin.Recovery(),
+	)
+	r.GET("/healthz", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"status": "ok"})
+	})
+
+	r.HTMLRender = &renderer.TemplRender{}
+	return &server{
+		r:   r,
+		cfg: cfg,
+	}, nil
+}
+
+func (s *server) Run() error {
+	return s.r.Run(fmt.Sprintf("%s:%d", s.cfg.IP, s.cfg.WebConfig.Port))
+}
